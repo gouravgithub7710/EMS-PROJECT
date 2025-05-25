d0da4bc52b8bd92d1990cf6f11425171aa2ab241
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-26 00:13:28 +0530

    Tailwind install

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-  
-      <div>
-        Hellooooooooooooooooooooooo ji
-      </div>
-      
-    
+    <div className='text-red-500 text-5xl font-bold '>
+      APP
+    </div>
   )
 }
 
