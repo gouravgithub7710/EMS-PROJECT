1e1d96850266aaf6365c09d63d3f26a7eb30d9b2
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-26 23:46:52 +0530

    header----Employee-dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Login from './components/Auth/Login'
+import EmployeeDashBoard from './components/DashBoard/EmployeeDashBoard'
 
 const App = () => {
   return (
-
-    //fragments--<></>
     <>
-<Login />
+    {/* <Login /> */}
+    <EmployeeDashBoard/>
     </>
   )
 }
