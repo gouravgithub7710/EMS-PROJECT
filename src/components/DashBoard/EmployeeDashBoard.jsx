c6f24310879f1c0b523f23afb0c84de9f17366bc
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-27 00:38:54 +0530

    TaskList

--- src/components/DashBoard/EmployeeDashBoard.jsx
+++ src/components/DashBoard/EmployeeDashBoard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
 import TaskListNumber from '../others/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashBoard = () => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
     <Header /> 
     <TaskListNumber /> 
+    <TaskList />
     </div>
   )
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 w-full py-5 flex-nowrap mt-10'>
+
+
+      <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-red-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+
+
+ <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+
+       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-green-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+
+
+       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+
+
+       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-red-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+
+       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
+      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      </div>
+     
+      
+    </div>
+  )
+}
+
+export default TaskList
