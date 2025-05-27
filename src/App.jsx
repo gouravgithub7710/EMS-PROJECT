36a12dc67b8207ec0fac15330f5b2b9dde5c481c
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-27 17:48:29 +0530

    Local storage set data and get data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashBoard from './components/DashBoard/EmployeeDashBoard'
 import AdminDashBoard from './components/DashBoard/AdminDashBoard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
+
+  
+  // useEffect(() => {
+  //   //setLocalStorage()
+  //   getLocalStorage()
+  // },)
+
+  const [user, setUser] = useState(null)
+  
   return (
     <>
-    {/* <Login /> */}
+    <Login />
     {/* <EmployeeDashBoard/> */}
-    <AdminDashBoard />
+    {/* <AdminDashBoard /> */}
     </>
   )
 }
