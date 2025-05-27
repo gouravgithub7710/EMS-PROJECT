b8b30578b47a291cf0dbec9b5b21e92a1f306a40
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-27 16:25:39 +0530

    AllTask complete

--- src/components/DashBoard/AdminDashBoard.jsx
+++ src/components/DashBoard/AdminDashBoard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
 import CreateTask from '../others/CreateTask'
+import AllTask from '../others/AllTask'
 
 const AdminDashBoard = () => {
   return (
     <div className='p-5 h-screen w-full'>
       <Header/>
       <CreateTask />
+      <AllTask  />
     </div>
   )
 }
---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-35 overflow-auto'>
+      <div className='flex items-center justify-between bg-emerald-900 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className='flex items-center justify-between bg-blue-400 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className='flex items-center justify-between bg-green-400 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className='flex items-center justify-between bg-yellow-400 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className='flex items-center justify-between bg-pink-400 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+
+      <div className='flex items-center justify-between bg-cyan-800 py-2 px-4 rounded mb-2 '>
+        <h2>Gourav</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
