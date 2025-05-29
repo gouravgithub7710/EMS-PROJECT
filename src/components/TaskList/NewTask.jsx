3a07f58a709026fa19bc0c9ffb8aa2b94a4ebcff
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-30 01:35:25 +0530

    NewTask

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const NewTask = () => {
   return (
-    <div>
-      
-    </div>
+    <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
+      <div className='flex justify-between items-center '>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
+      <h4 className='text-sm'>20 May 2025</h4>
+      </div>
+      <h2 className='mt-5 text-2xl font-semibold'>Create a Website</h2>
+      <p className='text-sm mt-5'>Design a personal portfolio showcasing your skills, projects, and contact info using modern frontend tools.</p>
+      <div className='mt-4'>
+        <button>
+        Accept Task
+        </button>
+      </div>
+      </div>
   )
 }
 
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,38 +1,41 @@
 import React from 'react'
 import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
 
 const TaskList = ({data}) => {
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 w-full py-5 flex-nowrap mt-10'>
    <AcceptTask />
 
-      <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-red-400 rounded-xl'>
+      {/* <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-red-400 rounded-xl'>
       <div className='flex justify-between items-center '>
       <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
       <h4 className='text-sm'>20 May 2025</h4>
       </div>
       <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
       <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
-      </div>
+      </div> */}
 
+<NewTask />
 
- <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
+ {/* <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
       <div className='flex justify-between items-center '>
       <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
       <h4 className='text-sm'>20 May 2025</h4>
       </div>
       <h2 className='mt-5 text-2xl font-semibold'>Create a Website</h2>
       <p className='text-sm mt-5'>Design a personal portfolio showcasing your skills, projects, and contact info using modern frontend tools.</p>
-      </div>
+      </div> */}
 
-       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-green-400 rounded-xl'>
+<CompleteTask />
+       {/* <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-green-400 rounded-xl'>
       <div className='flex justify-between items-center '>
       <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
       <h4 className='text-sm'>20 May 2025</h4>
       </div>
       <h2 className='mt-5 text-2xl font-semibold'>Use API to Fetch Data</h2>
       <p className='text-sm mt-5'>Fetch and display real-time data from a public API using JavaScript and display it on your page.</p>
-      </div>
+      </div> */}
 
 
        <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-fuchsia-700 rounded-xl'>
