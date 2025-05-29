798fcc027e815ab537200a266278a94b0ab1662f
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-05-29 23:54:47 +0530

    some changes in file

--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -4,7 +4,7 @@ const TaskListNumber = ({data}) => {
   return (
     <div className='flex screen mt-10 justify-between gap-5 screen'>
       <div className=' w-[45%] rounded-xl py-6 px-9 bg-red-400'>
-        <h2 className='text-3xl font-semibold'>{data.taskCounts.newTask}</h2>
+        <h2 className='text-3xl font-semibold'>{}</h2>
         <h3 className='text-xl font-medium '>New Task</h3>
       </div>
 
