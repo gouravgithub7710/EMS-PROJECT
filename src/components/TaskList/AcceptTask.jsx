ebad7bbe4dcd5c616f701b739a5707e84e3b67cd
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-01 01:38:10 +0530

    update fetch Data

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const AcceptTask = () => {
+const AcceptTask = ({data}) => {
+
+
+  
   return (
   
       <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-violet-700 rounded-xl'>
       <div className='flex justify-between items-center '>
-      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-      <h4 className='text-sm'>20 May 2025</h4>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>{data.category}</h3>
+      <h4 className='text-sm'>{data.taskDate}</h4>
       </div>
-      <h2 className='mt-5 text-2xl font-semibold'>Deploy a project</h2>
-      <p className='text-sm mt-5'>Deploy a web project online using platforms like Vercel, Netlify, or GitHub Pages easily.</p>
+      <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+      <p className='text-sm mt-5'>{data.taskDescription}</p>
       <div className='flex justify-between mt-4 '>
         <button className='bg-green-500 py-1 px-2 text-sm rounded'>Mark as Completed</button>
         <button className='bg-red-500 py-1 px-2 text-sm rounded'>Mark as Failed</button>
