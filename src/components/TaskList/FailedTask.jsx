c4de69d520d39a7a3eeac8e6e4dc975915beaba7
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-01 01:38:23 +0530

    update fetch Data

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const FailedTask = () => {
+const FailedTask = ({data}) => {
   return (
      <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-fuchsia-700 rounded-xl'>
       <div className='flex justify-between items-center '>
-      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-      <h4 className='text-sm'>20 May 2025</h4>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>{data.category}</h3>
+      <h4 className='text-sm'>{data.taskDate}</h4>
       </div>
-      <h2 className='mt-5 text-2xl font-semibold'>Host on GitHub Pages</h2>
-      <p className='text-sm mt-5'>Upload your static website to GitHub and make it live using GitHub Pages in minutes.</p>
+      <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+      <p className='text-sm mt-5'>{data.taskDescription}</p>
       
     <div className='mt-4 '>
         <button className='w-full bg-red-500 rounded py-1 items-center'>Failed</button>
