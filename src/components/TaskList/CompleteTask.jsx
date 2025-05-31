74b4a28469a2696c39333a8d85b4e33326cbea1c
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-01 01:38:16 +0530

    update fetch Data

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const CompleteTask = () => {
+const CompleteTask = ({data}) => {
   return (
      <div className=' h-full p-5 flex-shrink-0 w-[300px] bg-blue-400 rounded-xl'>
       <div className='flex justify-between items-center '>
-      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-      <h4 className='text-sm'>20 May 2025</h4>
+      <h3 className='bg-red-600 px-3 py-1 text-sm rounded'>{data.category}</h3>
+      <h4 className='text-sm'>{data.taskDate}</h4>
       </div>
-      <h2 className='mt-5 text-2xl font-semibold'>Use API to Fetch Data</h2>
-      <p className='text-sm mt-5'>Fetch and display real-time data from a public API using JavaScript and display it on your page.</p>
+      <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
+      <p className='text-sm mt-5'>{data.taskDescription}</p>
       <div className='mt-4 '>
         <button className='w-full bg-green-700 rounded py-1 items-center'>Complete</button>
       </div>
