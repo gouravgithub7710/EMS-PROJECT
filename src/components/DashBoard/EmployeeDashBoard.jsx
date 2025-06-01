4cd9ff4772a668b9c6f49a6e007da3a71b76a006
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-02 01:03:09 +0530

    EDB

--- src/components/DashBoard/EmployeeDashBoard.jsx
+++ src/components/DashBoard/EmployeeDashBoard.jsx
@@ -3,13 +3,13 @@ import Header from '../others/Header'
 import TaskListNumber from '../others/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashBoard = ({data}) => {
+const EmployeeDashBoard = (props) => {
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
     
-    <Header data={data} /> 
-    <TaskListNumber data={data}/> 
-    <TaskList data={data}/>
+    <Header changeUser={props.changeUser} data={props.data} /> 
+    <TaskListNumber data={props.data}/> 
+    <TaskList data={props.data}/>
     </div>
   )
 }
