36dc5c27fddd717c7cf173f43b4e4c376bc8c1b3
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-02 01:03:18 +0530

    ADB

--- src/components/DashBoard/AdminDashBoard.jsx
+++ src/components/DashBoard/AdminDashBoard.jsx
@@ -3,10 +3,10 @@ import Header from '../others/Header'
 import CreateTask from '../others/CreateTask'
 import AllTask from '../others/AllTask'
 
-const AdminDashBoard = () => {
+const AdminDashBoard = (props) => {
   return (
     <div className='p-5 h-screen w-full'>
-      <Header/>
+      <Header changeUser={props.changeUser}/>
       <CreateTask />
       <AllTask  />
     </div>
