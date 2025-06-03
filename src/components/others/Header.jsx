195440f704d8153349f068195d4dfb665eb3c1f5
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-06-04 00:29:40 +0530

    Header

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -3,6 +3,8 @@ import { setLocalStorage } from '../../utils/localStorage'
 
 const Header = (props) => {
   
+
+  
   const logOutUser = () => {
     localStorage.setItem('loggedInUser','')
     props.changeUser('')
@@ -11,7 +13,7 @@ const Header = (props) => {
   }
   return (
     <div className='flex items-end justify-between'>
-      <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>User👋</span> </h1>
+      <h1 className='text-2xl font-medium'>Hello <br/> <span className='text-3xl font-semibold'>UserName👋</span> </h1>
       <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
